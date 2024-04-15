e1e25aaf32e26beeaad9cc9a2978a4d5a2f1089a
JacobsonAaron <86175405+JacobsonAaron@users.noreply.github.com>
2024-04-14 23:55:56 -0400

    Remove unused code

--- src/CorMat/distances.py
+++ src/CorMat/distances.py
@@ -21,17 +21,6 @@ class distances():
         elif np.abs(val) <= zero_tol:
             return 0
     
-    # def BuresDistanceFromWikipedia(A, B, Ahalf=None, A_neghalf=None, zero_tol=10**-10):
-    #     """Recommended: Compute A^(1/2) (i.e. Ahalf) and pass that into the method. This will be faster for pairwise distance loops.
-    #     This version of this function mirrors the syntax currently found on Wikipedia."""
-    #     if Ahalf is None:
-    #         Ahalf = la.fractional_matrix_power(A, 1/2)
-    #     val = np.trace(A) + np.trace(B) - 2*np.sqrt(distances.BuresFidelity(A, B, Ahalf))
-    #     if np.abs(val) > zero_tol:
-    #         return np.sqrt(val).real # NOTE: Switching order of A and B inputs appears to only meaningfully affect imaginary part.
-    #     elif np.abs(val) <= zero_tol:
-    #         return 0
-    
     def RootBuresFidelity(A, B, Ahalf=None, A_neghalf=None, featureDist=None):
         """Returns the square root of the Bures fidelity between A and B. If tr(a)==tr(B)==1, A and B represent states, then suitable for BuresAngle.
         See: https://arxiv.org/pdf/1712.01504.pdf. A Wikipedia article exists as well."""
