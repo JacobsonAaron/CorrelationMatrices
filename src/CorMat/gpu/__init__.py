aac9adf50f27af4008f07f6f19eace6f19c897dd
Aaron Jacobson <AaronMorauski@gmail.com>
2025-01-26 23:38:21 -0500

    Updated dependencies and config info

--- src/CorMat/gpu/__init__.py
+++ src/CorMat/gpu/__init__.py
@@ -1,7 +1,5 @@
 __all__ = ["__version__", "linalg", "distances", 'utils']
 
-import os
-
 # Use if files have classes inside; this imports as Object (from within files)
 # Allows access as CorMat.plots.Plots or as CorMat.Plots
 from .linalg import linalg
